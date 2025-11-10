bfad93fd4da8c0309400c66fffd130d68b4a55f5
maged-web <maged.mamdouh@brightcreations.com>
2025-11-10 12:50:00 +0200

    Updates

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,10 +6,11 @@ import { User } from './user.entity';
 import { AuthProvider } from './auth.provider';
 import { MailModule } from 'src/mail/mail.module';
 import { UserEventsListener } from './events/user-events.listener';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), MailModule],
+    imports: [TypeOrmModule.forFeature([User]), MailModule, EventEmitterModule.forRoot()],
 
     controllers: [UserController],
     providers: [UserService, AuthProvider, UserEventsListener],
